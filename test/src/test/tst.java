85c9e18a750379e80e1baeeeb7472e9205cb49e0
aaa <aaa@LAPTOP-LPBCCOG1>
2023-09-26 16:01:23 +0200

    seconda versione

--- test/src/test/tst.java
+++ test/src/test/tst.java
@@ -9,9 +9,10 @@ public class tst {
 		
 			try {
 				FileWriter fw=new FileWriter("prodotti.txt"); //crea un file di nome prodotti.txt
-				fw.write("Pane\n");  //scrive una riga nel file
-				fw.write("Pasta\n");
-				fw.write("grissini");
+				fw.write("Pane;3.50;20\n");  //scrive una riga nel file
+				fw.write("Pasta;1.60;45\n");
+				fw.write("Biscotti;4.70;80\n");
+				fw.write("Salame;35;37\n");;
 				fw.close();  //chiude il file
 			    
 				System.out.println("File corretamente creato");
@@ -22,14 +23,19 @@ public class tst {
 				e.printStackTrace();
 			}
 		File f=new File("prodotti.txt");
+		String[] dati;
 		
 		try {
 			Scanner sc=new Scanner(f);  //questo scnnner non legge dalla tastiera bensi dal file f
 			String riga;
 			while (sc.hasNextLine()){  //ripeti finche ci sono righe nel file
 				riga=sc.nextLine();    //leggi una riga dal file
-				
-				System.out.println("Trovato prodotto:"+riga);
+				dati=riga.split(";");  //divide la stringa in piu pezzi usando la stringa come separatore
+				System.out.println("Trovato prodotto:");
+				System.out.println("Nome:"+dati[0]);
+				System.out.println("Prezzo:"+dati[1]);
+				System.out.println("quantità:"+dati[2]);
+				System.out.println("\n");
 			}
 			sc.close(); //chiudi il file
 			System.out.println("\n\n Lettura terminata");
